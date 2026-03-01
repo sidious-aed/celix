3a1830714f4301442d584e0cb73b2d87ed2ac8b4
sidious <greanzar@gmail.com>
2026-02-28 23:46:56 -0700

    refined naof-grid-secs to initial expansion spec. refined equations-main testing.

--- asms/equations/main.asm
+++ asms/equations/main.asm
@@ -1,5 +1,11 @@
+##########################################################################################################
+# equations-main
+##########################################################################################################
 % equations
 % views
+##########################################################################################################
+# init
+##########################################################################################################
 sub 1000 rsp
 aqs equations
 mq r11 equations
@@ -12,7 +18,11 @@ mov 2a r8
 mq r8 naof-i-sim-secs
 aqs i-sim-2
 isr 100
+ent nao-relay space
 
+##########################################################################################################
+# com
+##########################################################################################################
 lent i-sim rsi
 lq i-sim-2 rdi
 mq naof-i-sim-secs rcx
@@ -26,6 +36,9 @@ mov 2a rdx
 mov 1 rax
 sys
 
+##########################################################################################################
+# number-to-entree
+##########################################################################################################
 aqs naof-secs
 aqs b16-entree
 isr 100
@@ -49,6 +62,9 @@ mov 1 rdx
 mov 1 rax
 sys
 
+##########################################################################################################
+# entre-to-number
+##########################################################################################################
 aqs b16-number
 ent b16-nentree aed27
 lent b16-nentree rsi
@@ -127,18 +143,25 @@ mov rax rdx
 mov 1 rax
 sys
 
+##########################################################################################################
+# naof-grid-secs
+##########################################################################################################
 aqs grid
 isr 10
 lq grid rdi
 nao r9
 mov r9 0 rdi
 mov 100 rsi
+#mov 989680 rsi
 mq equations r11
 addc equations naof-grid-secs r11
 dct r11
 
-ent nao-relay number
-lent nao-relay rsi
+##########################################################################################################
+# view-number
+##########################################################################################################
+ent grid-relay grid-site
+lent grid-relay rsi
 mq grid rdi
 #mov aedaed rdi
 mov 10 rbx
@@ -147,6 +170,31 @@ mq views r11
 addc views view-number r11
 dct r11
 
+ent grid-at-relay grid-at
+lent grid-at-relay rsi
+lq grid rdi
+mov 8 rdi rdi
+#mov aedaed rdi
+mov 10 rbx
+mq equations rdx
+mq views r11
+addc views view-number r11
+dct r11
+
+ent grid-breadth-relay grid-breadth
+lent grid-breadth-relay rsi
+lq grid rdi
+mov 10 rdi rdi
+#mov aedaed rdi
+mov 10 rbx
+mq equations rdx
+mq views r11
+addc views view-number r11
+dct r11
+
+##########################################################################################################
+# view-space
+##########################################################################################################
 aqs space0
 isr 18
 lq space0 r9
@@ -166,6 +214,94 @@ addc views view-space r11
 dct r11
 #init
 #com
+
+##########################################################################################################
+# grid-expansion
+##########################################################################################################
+ent grid-i-sim talkei talkei. airgo vah nah goo trim brader.\n
+aqs naof-grid-i-sim-secs
+mov 2e r8
+mq r8 naof-grid-i-sim-secs
+aqs esite
+nao r8
+mq r8 esite
+aqs current-memo
+aqs init-current-memo
+mq r8 init-current-memo
+
+s grid-expansion-init
+	mq esite r8
+	cmp 67 r8
+	st je grid-expansion-com
+	lq grid rdi
+	mov 2710 rsi
+	mq equations r11
+	addc equations naof-grid-secs r11
+	dct r11
+	mq rax current-memo
+
+	mq init-current-memo r8
+	cmp 0 r8
+	st jne initcm-com
+	mq rax init-current-memo
+	s initcm-com
+
+	lent grid-i-sim rsi
+	mq current-memo rdi
+	mq naof-grid-i-sim-secs rcx
+	mq equations r11
+	addc equations com r11
+	dct r11
+
+	mov 1 rdi
+	mq current-memo rsi
+	mq naof-grid-i-sim-secs rdx
+	mov 1 rax
+	sys
+
+	mq esite r8
+	add 1 r8
+	mq r8 esite
+	st jmp grid-expansion-init
+s grid-expansion-com
+
+lent grid-relay rsi
+mq grid rdi
+mov 10 rbx
+mq equations rdx
+mq views r11
+addc views view-number r11
+dct r11
+
+lent grid-at-relay rsi
+lq grid rdi
+mov 8 rdi rdi
+#mov aedaed rdi
+mov 10 rbx
+mq equations rdx
+mq views r11
+addc views view-number r11
+dct r11
+
+lent grid-breadth-relay rsi
+lq grid rdi
+mov 10 rdi rdi
+#mov aedaed rdi
+mov 10 rbx
+mq equations rdx
+mq views r11
+addc views view-number r11
+dct r11
+
+mov 1 rdi
+mq init-current-memo rsi
+mq naof-grid-i-sim-secs rdx
+mov 1 rax
+sys
+
+mq equations r11
+addc equations task r11
+#dct r11
 
 add 1000 rsp
 ret
--- asms/equations/naof-grid-secs.asm
+++ asms/equations/naof-grid-secs.asm
@@ -7,7 +7,7 @@
 #       2 | breadth
 # rsi | naof-secs
 ##########################################################################################################
-# com-init
+# init
 ##########################################################################################################
 sub 1000 rsp
 
@@ -15,62 +15,116 @@ aqs grid
 mq rdi grid
 aqs naof-secs
 mq rsi naof-secs
+aqs expansion-facter
+mov 2 r13
+mq r13 expansion-facter
+aqs naof-mmap-secs
+nao r8
+mq r8 naof-mmap-secs
+ent init-entree initialising grid.\n
+aqs naof-init-entree-secs
+mov 13 r8
+mq r8 naof-init-entree-secs
+ent expansion-entree expanding grid.\n
+aqs naof-expansion-entree-secs
+mov 10 r8
+mq r8 naof-expansion-entree-secs
+aqs grid-at
+mq r8 grid-at
+aqs grid-breadth
+mq r8 grid-breadth
 
+##########################################################################################################
+# grid-init
+##########################################################################################################
 mq grid r8
 mov 0 r8 r8
+mq r8 grid-at
 cmp 0 r8
 st jne init-grid-com
 s init-grid-init
-mq grid r11
-# anonomous-mmap
-nao rdi
-mov rdi 8 r11
-mov f4240 rsi
-mq rsi 10 r11
-mov 7 rdx
-mov 22 r10
-nao r9
-nao r8
-mov 9 rax
-sys
-mq grid r11
-mov rax 0 r11
+cmp f4240 rsi
+st jb custom-init-naof-secs-com
+add 1 rsi
+factq expansion-facter rsi
+st jmp init-naof-secs-com
+s custom-init-naof-secs-com
+	mov f4240 rsi
+	s init-naof-secs-com
+	mq rsi grid-breadth
+
+	mov 1 rdi
+	lent init-entree rsi
+	mq naof-init-entree-secs rdx
+	mov 1 rax
+	sys
+
+	mq grid-breadth rsi
+	mq rsi naof-mmap-secs
+	mq grid r11
+	mov rsi 10 r11
+	# anonomous-mmap
+	nao rdi
+	mov rdi 8 r11
+	mov 7 rdx
+	mov 22 r10
+	nao r9
+	nao r8
+	mov 9 rax
+	sys
+	mq grid r11
+	mov rax 0 r11
 s init-grid-com
 
+##########################################################################################################
+# grid-expansion
+##########################################################################################################
 mq grid r11
 mov 8 r11 r8
 mq naof-secs r9
 add r8 r9
+#mq r9 grid-at
 mov 10 r11 r10
-cmp r9 r10
+mq r10 grid-breadth
+cmp r10 r9
 st jb expansion-com
+
 s expansion-init
-aqs expansion-facter
-mov 2 r13
-mq r13 expansion-facter
-# anonomous-mmap
-nao rdi
-mov r9 rsi
-factq expansion-facter rsi
-mov rsi 8 r11
-mov 7 rdx
-mov 22 r10
-nao r9
-nao r8
-mov 9 rax
-sys
-mq grid r11
-mov rax 0 r11
+	mov 1 rdi
+	lent expansion-entree rsi
+	mq naof-expansion-entree-secs rdx
+	mov 1 rax
+	sys
+
+	mq grid r11
+	mov 10 r11 r10
+	# anonomous-mmap
+	nao rdi
+	mov r10 rsi
+	mq naof-secs r8
+	add r8 rsi
+	factq expansion-facter rsi
+	mov rsi 10 r11
+	mq rsi naof-mmap-secs
+	mov 7 rdx
+	mov 22 r10
+	nao r9
+	nao r8
+	mov 9 rax
+	sys
+	mq grid r11
+	mov rax 0 r11
+	nao r8
+	mov r8 8 r11
 s expansion-com
 
 mq grid r11
-mov 10 r11 r9
-mq naof-secs r10
-add r10 r9
-mov r9 10 r11
 mov 0 r11 rax
 mov 8 r11 r8
-add r8 rax
+mq naof-secs r9
+add r9 r8
+mov r8 8 r11
+add r9 rax
 ##########################################################################################################
 # com-com
 ##########################################################################################################
@@ -94,10 +148,27 @@ mq rax file
 # write
 mq file rdi
 mov 8 rdx
-mq naof-secs rdx
-mq entree rsi
+lq naof-secs rsi
+mov 1 rax
+#sys
+# write
+mq file rdi
+mov 8 rdx
+lq naof-mmap-secs rsi
+mov 1 rax
+#sys
+# write
+mq file rdi
+mov 8 rdx
+lq grid-at rsi
 mov 1 rax
 sys
+# write
+mq file rdi
+mov 8 rdx
+lq grid-breadth rsi
+mov 1 rax
+#sys
 # close
 mq file rdi
 mov 3 rax
